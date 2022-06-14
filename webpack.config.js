7143ec000c0ed144b27d6f90bb0402df8abd581c
maru5mango <mjel0507@gmail.com>
2022-06-14 18:27:28 +0900

    [Continue]need to setting webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
+const path = require("path");
+const webpack = require("webpack");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 const HtmlwebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
-const { resolve } = require("path");
+const { resolve } = path;
 
 // html-webpack-plugin : output html
 // mini-css-extract-plugin: output css
@@ -36,7 +41,26 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [{ from: "static", to: "static" }],
     }),
+    new webpack.DefinePlugin({
+      "process.env.mode": JSON.stringify(env.mode),
+    }),
+    new UglifyJSPlugin({
+      test: /\.js?$/i,
+      keep_classnames: true,
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static",
+      reportFilename: "bundle-report.html",
+      openAnalyzer: false,
+      generateStatsFile: true,
+      statsFilename: "bundle-stats.json",
+    }),
   ],
+  resolve: {
+    alias: {
+      "@": path.join(__dirname, "src", "src"),
+    },
+  },
   module: {
     rules: [
       {
@@ -57,4 +81,12 @@ module.exports = {
       },
     ],
   },
+  optimization: {
+    minimize: true,
+    concatenateModules: true,
+    chunkIds: "size",
+  },
+  mode: "production",
+  watch: false,
+  devtool: "inline-source-map",
 };
